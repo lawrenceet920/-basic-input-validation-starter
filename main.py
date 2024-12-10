386ce26e6ef5ac41fed6a66b3b83a9797f87b9f0
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-12-10 13:10:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,19 @@
 # Are you and your partner working on Level 1, Level 2 or Level 3 today?
 # Working on Level...
 
+while True: # repeat code
+    # Input
+    userinput = input('Please enter a 6-10 digit number:        ')
+
+    # Process
+    if userinput.isdigit():
+        if  5 < len(userinput) < 11:
+            output = f'Valid input: {userinput}'
+            break
+        else:
+            output = 'Input must be between 6 and 10 characters long.'
+    else:
+        output = 'Input must be numeric'
+
+    # Output
+    print(output)
