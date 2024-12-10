607cd2fa0b98b64f2502c13046907a368f671a3d
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-12-10 13:12:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# Your name and your partner's name
-# Current date
+# Braden Leach & Ethan Lawrence
+# dec 10 2024
 # Validating String Input (Tiered Assignment)
 
 # Are you and your partner working on Level 1, Level 2 or Level 3 today?
