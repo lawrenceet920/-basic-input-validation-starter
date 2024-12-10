c4ee7fca8b3fc6842bf4f80de7887abd112aaa40
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-12-10 12:54:48 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Your name and your partner's name
+# Current date
+# Validating String Input (Tiered Assignment)
+
+# Are you and your partner working on Level 1, Level 2 or Level 3 today?
+# Working on Level...
+
